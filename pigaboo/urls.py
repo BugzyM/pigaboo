61262246eeffcbf8235b605c354fe2acf61911e5
Avukonke Peter <avukonke@gmail.com>
2016-01-06 15:58:45 +0200

    Add base Pigaboo apps directory, update urls

---
+++ pigaboo/urls.py
@@ -0,0 +1,38 @@
+"""pigaboo URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+
+    url(r'^$', 'pigaboo.apps.pigaboo_app.views.pg_login_user', name='login'),
+    url(r'^pg_account/', 'pigaboo.apps.pigaboo_app.views.pg_account',
+        name='pg_account'),
+    url(r'^pg_upload/', 'pigaboo.apps.pigaboo_app.views.pg_upload',
+        name='pg_upload'),
+    #url(r'^blog/', include('blog.urls')),
+
+]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL,
+#                          document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL,
+#                          document_root=settings.MEDIA_ROOT)
